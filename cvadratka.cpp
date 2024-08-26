b6c3efd9a10a7512b91d40a35e5d9a7ba34f3c75
asbolt <asboltova@gmail.com>
2024-08-26 14:24:30 +0300

    Update cvadratka.cpp

--- cvadratka.cpp
+++ cvadratka.cpp
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <stdlib.h>
 // TODO отдельные файлы
-// TODO выбор тесты или ввод вручную
+
 enum AmountRoots 
 {
     INFINITY_ROOTS = -2, 
@@ -26,10 +26,8 @@ enum Compare
     EQUAL = 0
 };
 
-
 const double EPS = 1e-5;
 const int COEFF_ARRAY_SIZE = 3;
-
 
 struct Coefficient
 {
@@ -43,7 +41,7 @@ struct Roots
     double x2 = NAN; 
 };
 
-struct Tests
+struct Test
 {
     int number = -1;
     Coefficient coefficient[3] = {{NAN, 'x'}, {NAN, 'x'}, {NAN, 'x'}};
@@ -52,7 +50,7 @@ struct Tests
     int nRootsExpected = NULL; 
 };
 
-const Tests TESTS [] = 
+const Test TESTS [] = 
 {
     {0, {{1}, {2}, {-3}}, 1, -3, TWO_ROOTS}, // d > 0
     {1, {{2}, {4}, {2}}, -1, -1, TWO_SAME_ROOTS}, // d = 0
@@ -60,8 +58,10 @@ const Tests TESTS [] =
     {3, {{0}, {4}, {-8}}, 2, NAN, ONE_ROOT}, // Linear
     {4, {{0}, {0}, {56}}, NAN, NAN, NULL_ROOTS}, // NULL
     {5, {{0}, {0}, {0}}, NAN, NAN, INFINITY_ROOTS}, // INF
-    {6, {{1.678}, {5.829}, {2.443}}, -0.818085, -5.010915, TWO_ROOTS}, // fractional numbers
+    {6, {{1.678}, {5.829}, {2.443}}, -0.487536,  -2.98624, TWO_ROOTS}, // fractional numbers
 };
+
+const int testsAmount = sizeof(TESTS)/sizeof(TESTS[0]);
 
 int compareToZero (double coefficient);
 int scanNumber (Coefficient *coefficient);
@@ -70,26 +70,45 @@ int printRoots (Coefficient coefficient [], Roots *roots);
 int solveLinear (Coefficient coefficient [], struct Roots *roots);
 int solveQuadratic (Coefficient coefficient [], struct Roots *roots);
 int solve (Coefficient coefficient [], struct Roots *roots);
-int printTestResults(struct AllValues);
+int printBadTestResults(struct AllValues);
+int printGoodTestResults(Roots roots, Test test);
 int checkTestResults(struct AllValues);
-int test (Tests test);
+int test (Test test);
 
 int main ()
 {
     Coefficient coefficient [COEFF_ARRAY_SIZE] = { {NAN, 'a'}, {NAN, 'b'}, {NAN, 'c'} };
     Roots roots = {NAN, NAN};
 
-    if (enterNumbers(coefficient) != SUCCESS) 
+    printf ("Введите \"t\", если хотите запустить тесты, \n");
+    printf ("или введите \"s\", если хотите ввести коэффициенты вручную.\n");
+
+    char letter = 'x';
+    if (scanf ("%c", &letter) == 1 && (letter == 't' || letter == 'T' || letter == 's' || letter == 'S'))
     {
-        return ERROR;
-    }
+        if (letter == 't' || letter == 'T')
+        {
+            for (int i = 0; i < testsAmount; i++)
+            test (TESTS [i]);
+
+            return 0;
+        }
+        else
+        {
+            if (enterNumbers(coefficient) != SUCCESS) 
+            {
+                return ERROR;
+            }
     
-    printRoots (coefficient, &roots);
-
-    for (int i = 0; i < 5; i++)
-        test (TESTS [i]);
-
-    return 0;
+            printRoots (coefficient, &roots);
+        }
+    }
+    else 
+    {
+        while (getchar () != '\n') {}
+        printf ("Ошибка ввода");
+        return SUCCESS;
+    }
 }
 
 int compareToZero (double coefficient) 
@@ -231,42 +250,62 @@ int solve (Coefficient coefficient [], Roots *roots)
    } 
 }
 
-int printTestResults(Roots roots, Tests tests) 
+int printBadTestResults(Roots roots, Test test) 
 {
     
     printf ("Error Test %d: a = %lg, b = %lg, c = %lg, x1 = %lg, x2 = %lg, nRoots = %d \nExpected: x1 = %lg, x2 = %lg, \
-             nRoots = %d\n", tests.number, tests.coefficient[0].value, tests.coefficient[1].value, tests.coefficient[2].value,
-             roots.x1, roots.x2, solve (tests.coefficient, &roots), tests.x1Expected, tests.x2Expected, tests.nRootsExpected);
+             nRoots = %d\n", test.number, test.coefficient[0].value, test.coefficient[1].value, test.coefficient[2].value,
+             roots.x1, roots.x2, solve (test.coefficient, &roots), test.x1Expected, test.x2Expected, test.nRootsExpected);
     return SUCCESS;
 }
 
-int checkTestResults(Roots roots, Tests tests)
+int printGoodTestResults(Roots roots, Test test) 
 {
-   switch (solve (tests.coefficient, &roots))
+    
+    printf ("Test %d: a = %lg, b = %lg, c = %lg, x1 = %lg, x2 = %lg, nRoots = %d\n", test.number, test.coefficient[0].value,
+            test.coefficient[1].value, test.coefficient[2].value, roots.x1, roots.x2, solve (test.coefficient, &roots));
+    return SUCCESS;
+}
+
+int checkTestResults(Roots roots, Test test)
+{
+   switch (solve (test.coefficient, &roots))
     {
         case INFINITY_ROOTS:
         case NULL_ROOTS:
-            if (solve (tests.coefficient, &roots) != tests.nRootsExpected)
+            if (solve (test.coefficient, &roots) != test.nRootsExpected)
             {
-                printTestResults(roots, tests);
+                printBadTestResults(roots, test);
+            }
+            else
+            {
+                printGoodTestResults(roots, test);
             }
             return SUCCESS;
 
         case ONE_ROOT:
             // TODO check isnan and isinf
             // TODO write function to check if xn is equal to xnExpected
-            if (solve (tests.coefficient, &roots) != tests.nRootsExpected || compareToZero(roots.x1 - tests.x1Expected) != EQUAL)
+            if (solve (test.coefficient, &roots) != test.nRootsExpected || compareToZero(roots.x1 - test.x1Expected) != EQUAL)
             {
-                printTestResults(roots, tests);
+                printBadTestResults(roots, test);
+            }
+            else
+            {
+                printGoodTestResults(roots, test);
             }
             return SUCCESS;
 
         case TWO_ROOTS:
         case TWO_SAME_ROOTS:
-            if (solve (tests.coefficient, &roots) != tests.nRootsExpected || compareToZero(roots.x1 - tests.x1Expected) != EQUAL 
-                || compareToZero(roots.x2 - tests.x2Expected) != EQUAL)
+            if (solve (test.coefficient, &roots) != test.nRootsExpected || compareToZero(roots.x1 - test.x1Expected) != EQUAL 
+                || compareToZero(roots.x2 - test.x2Expected) != EQUAL)
             {
-                printTestResults(roots, tests);
+                printBadTestResults(roots, test);
+            }
+            else
+            {
+                printGoodTestResults(roots, test);
             }
             return SUCCESS;
 
@@ -276,13 +315,13 @@ int checkTestResults(Roots roots, Tests tests)
     }
 }
 
-int test (Tests tests) 
+int test (Test test) 
 {
     double x1 = NAN, x2 = NAN;
     int number = 0;
 
     Roots roots = {x1, x2};
    
-    checkTestResults(roots, tests);
+    checkTestResults(roots, test);
     return SUCCESS;
 }
